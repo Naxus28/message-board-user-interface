3c9e4b3aee9e3b98adef24abd4a6f9b844bc7cb5
Gabriel Ferraz <gabrielferraz27@gmail.com>
2017-05-19 10:53:22 -0400

    implements directive for images on sidebar

---
+++ src/app/directives/sidebar-contacts/sidebar-contacts.directive.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+  angular
+    .module('directives')
+    .directive('sidebarContactsDirective', sidebarContactsDirective);
+
+  /* @nginject */
+  function sidebarContactsDirective () {
+    return {
+      restrict: '',
+      link: link,
+      replace: true,
+      scope: {
+        contacts: '='
+      },
+      templateUrl: 'app/directives/sidebar-contacts/sidebar-contacts.html'
+    };
+
+    function link(scope) {
+      scope.contacts = scope.contacts;
+    }
+  }
+})();
