7260baa0f24a6d82d74064b50af507a6bbf146df
Gabriel Ferraz <gabrielferraz27@gmail.com>
2017-05-20 13:53:53 -0400

    finishes toggle nav functionality

---
+++ src/app/services/authentication.service.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+  angular
+    .module('services')
+    .factory('authenticationService', authenticationService);
+
+  /* @ngInject */
+  function authenticationService() {
+
+    // mocks a backend login API (using 'id' for dual factor authentication instead of password)
+    let getUser = (users) => _.find(users, (user) => user.name === 'Josh' && user.id === 1); 
+    
+    const service = { 
+      getUser
+    };
+
+    return service;
+  }
+})();
