082c284816386408d1e922f8fb14b100a9ca04fd
Gabriel Ferraz <gabrielferraz27@gmail.com>
2017-05-22 11:40:23 -0400

    styles error page

--- src/app/error/error.controller.js
+++ src/app/error/error.controller.js
@@ -5,18 +5,6 @@
     .module('error')
     .controller('ErrorCtrl', ErrorCtrl);
 
-  function ErrorCtrl(circleProgressService) {
-    let vm = this;
-    let circleOptions = {
-      value: 1,
-      size: 145,
-      thickness: 10,
-      fill: {
-        gradient: ['#008CBA', '#D4B193', '#E7FCFC', '#29704D']
-      }
-    };
-
-    circleProgressService.setCircleOptions(circleOptions);
-    vm.circleOptions = circleProgressService.getCircleOptions();
+  function ErrorCtrl() {
   }
 })();
